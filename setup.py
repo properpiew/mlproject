30518c7fe70c5cc41b04c3a9490a9e5d22fb438f
properpiew <guptaaman515@gmail.com>
2025-06-14 16:56:18 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> list[str]:
+    
+    """
+    This function reads a requirements file and returns a list of packages.
+    It removes any comments or empty lines.
+    """
+    
+    requirements= []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace('\n', '') for req in requirements]
+    
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Umang',
+    author_email='guptaaman515@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
